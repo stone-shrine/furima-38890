16a9e3e8dc2f21bb5abf332e476da9f63edfd9e5
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-02 15:19:43 +0900

    アクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,21 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status, :payment, :prefecture, :lead_time, :price)
   end
 end
