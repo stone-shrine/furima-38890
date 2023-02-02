2e5a51514eb251ec0066d006824aadd9a26a5fff
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-02 14:52:01 +0900

    アソシエーションとバリデーションの設定

---
+++ app/models/item.rb
@@ -0,0 +1,33 @@
+class Item < ApplicationRecord
+# ActiveHashのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :payment
+  belongs_to :prefecture
+  belongs_to :lead_time
+# /ActiveHashのアソシエーション
+
+# ActiveHash以外のアソシエーション
+  belongs_to :user
+# /ActiveHash以外のアソシエーション
+
+
+# ActiveHashのバリデーション
+with_options presence: true, numericality: {other_than: 1, message: "can't be blank"} do
+  validates :category_id
+  validates :status_id
+  validates :prefecture_id
+  validates :payment_id
+  validates :lead_time_id
+end
+# /ActiveHashのバリデーション
+
+# ActiveHash以外のバリデーション
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+# /ActiveHash以外のバリデーション
+end
