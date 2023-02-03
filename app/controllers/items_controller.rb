c1ec692233d9890b1fff89ce02e04925e68aa828
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-03 21:14:34 +0900

    編集保存時のエラーの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,9 +29,11 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path(item)
+    else
+      render :edit
     end
   end
 
