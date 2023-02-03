e4ffec6c168a6ab6bb4f6df3ccb7022352120d4c
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-03 20:46:27 +0900

    ルーティングとアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, only: [:new, :edit]
+
+
   def index
     @items = Item.all.order('created_at DESC')
   end
 
   def new
-    redirect_to new_user_session_path unless user_signed_in?
     @item = Item.new
   end
 
@@ -21,10 +23,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  private
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user.id
+  end
 
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path(item)
+    end
+  end
+
+
+  private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :payment_id, :prefecture_id,
                                  :lead_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
