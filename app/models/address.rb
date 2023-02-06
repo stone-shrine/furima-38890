d99d044086a12237597d59b57845889b19771ba3
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-06 12:22:51 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+  with_options presence: true do
+    validates :zip_code
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :street
+    validates :phone
+  end
 end
