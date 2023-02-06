cb2b26eef016d5fba69e2466ad51a441d25e32ef
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-06 17:06:47 +0900

    Formオブジェクトの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,26 +3,22 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-  end
-
-  def new
+    @order_address = OrderAddress.new
   end
 
   def create
-    if @order = Order.create(order_params)
-      Address.create(address_params)
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
       redirect_to root_path
     else
-      render "/items/#{@order.item.id}/orders"
+      @item = Item.find(params[:item_id])
+      render :index
     end
   end
 
   private
   def order_params
-    params.permit.merge(user_id: current_user.id, item_id: params[:item_id])
-  end
-
-  def address_params
-    params.permit(:zip_code, :prefecture_id, :city, :street, :building, :phone).merge(order_id: @order.id)
+    params.require(:order_address).permit(:zip_code, :prefecture_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
