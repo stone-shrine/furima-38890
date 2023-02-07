c55a46d4d4741898b63e55230c94368e8f532037
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-07 11:03:24 +0900

    コントローラーのアクションの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
