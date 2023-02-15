8ec30834c8121b6d8d007ba1d6de8d16a4435ca7
stone-shrine <iso.5565.shnk@gmail.com>
2023-02-15 12:46:32 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.1'
 set :application, 'furima-38890'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'stone-shrine/furima-38890.git'
+set :repo_url,  'git@github.com:stone-shrine/furima-38890.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
